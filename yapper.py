2688f2981871e2a3d67af964ea501259d3d97581
Howard Walz <howardwalz@gmail.com>
2026-01-10 14:18:23 -0800

    using defualt timing params to avoid an error

--- yapper.py
+++ yapper.py
@@ -4,21 +4,10 @@ from canlib.canlib import ChannelData
 def setUpChannel(channel=0,
                  openFlags=canlib.Open.ACCEPT_VIRTUAL,
                  outputControl=canlib.Driver.NORMAL):
-    ch = canlib.openChannel(channel, openFlags)
+    ch = canlib.openChannel(channel, openFlags, bitrate=canlib.Bitrate.BITRATE_250K)
     print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
                                           ChannelData(channel).card_upc_no))
     ch.setBusOutputControl(outputControl)
-    # Specifying a bus speed of 500 kbit/s. See documentation
-    # for more informationon how to set bus parameters.
-    params = canlib.busparams.BusParamsTq(
-        tq=8,
-        phase1=2,
-        phase2=2,
-        sjw=1,
-        prescaler=40,
-        prop=3
-    )
-    ch.set_bus_params_tq(params)
     ch.busOn()
     return ch
 
